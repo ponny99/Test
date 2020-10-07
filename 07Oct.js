71f39abcf0534a73d50535770d9839bbb3c22084
Selfmade20e <tebogo1686@gmail.com>
2020-10-07 13:40:41 +0200

    completed the first task and adding it to git hub

---
+++ 07Oct.js
@@ -0,0 +1,11 @@
+// assign a variable name then assign it to a new name 
+var name= "Ponatshego"
+    name="Ramagaga"
+    console.log(name) 
+
+
+// split the following string : Welcome to this javascript Guide! and join it 
+
+// multiply  all the numbers in array by 4
+
+// print out numbers from 0 to 20....
