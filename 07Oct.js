e3bcee40dda827503ed47b00b614e3ce788fd9d5
Selfmade20e <tebogo1686@gmail.com>
2020-10-07 16:01:25 +0200

    uncommenting the tasks

--- 07Oct.js
+++ 07Oct.js
@@ -1,7 +1,7 @@
 // assign a variable name then assign it to a new name 
-// var name= "Ponatshego"
-//     name="Ramagaga"
-//     console.log(name) 
+var name= "Ponatshego"
+    name="Ramagaga"
+    console.log(name) 
 
 // split the following string : Welcome to this javascript Guide! and join it 
 var sentence = "Welcome to this javascript Guide!"
@@ -9,31 +9,31 @@ var sentence = sentence.split("").join();
 console.log(sentence)
 
 // multiply  all the numbers in an array by 4
-// var numbers=[2,4,6,8]
-// var mulplied=[]
-// for (var i = 0; i < numbers.length; i++){
-//     console.log(numbers[i] * 4)
-//  numbers[i] * 4;
+var numbers=[2,4,6,8]
+var mulplied=[]
+for (var i = 0; i < numbers.length; i++){
+    console.log(numbers[i] * 4)
+ numbers[i] * 4;
 
-// OR
+OR
 
 
-// var numbers=[2,4,6,8]
-// var multiplied=[]
-// for (var i = 0; i < numbers.length; i++){
-// multiplied.push(numbers[i] * 4)
+var numbers=[2,4,6,8]
+var multiplied=[]
+for (var i = 0; i < numbers.length; i++){
+multiplied.push(numbers[i] * 4)
 
-// }
-// console.log(multiplied)
+}
+console.log(multiplied)
 
 
 // print out numbers from 0 to 20....
     
-//  let n = 0;
+ let n = 0;
 
-// while (n <= 20) {
-//   console.log(n);
-//   n++;
-// }
+while (n <= 20) {
+  console.log(n);
+  n++;
+}
 
-// console.log('')
+console.log('')
