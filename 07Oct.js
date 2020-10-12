c79337c50cc7776c664116405daf6fac28eb4e52
Selfmade20e <tebogo1686@gmail.com>
2020-10-12 11:32:13 +0200

    adding the other activities I did

--- 07Oct.js
+++ 07Oct.js
@@ -1,39 +1,56 @@
 // assign a variable name then assign it to a new name 
-var name= "Ponatshego"
-    name="Ramagaga"
-    console.log(name) 
+// var name= "Ponatshego"
+//     name="Ramagaga"
+//     console.log(name) 
 
 // split the following string : Welcome to this javascript Guide! and join it 
-var sentence = "Welcome to this javascript Guide!"
-var sentence = sentence.split("").join();
-console.log(sentence)
+// var sentence = "Welcome to this javascript Guide!"
+// var sentence = sentence.split("").join();
+// console.log(sentence)
 
 // multiply  all the numbers in an array by 4
-var numbers=[2,4,6,8]
-var mulplied=[]
-for (var i = 0; i < numbers.length; i++){
-    console.log(numbers[i] * 4)
- numbers[i] * 4;
+// var numbers=[2,4,6,8]
+// var mulplied=[]
+// for (var i = 0; i < numbers.length; i++){
+//     console.log(numbers[i] * 4)
+//  numbers[i] * 4;
 
-OR
+// OR
 
 
-var numbers=[2,4,6,8]
-var multiplied=[]
-for (var i = 0; i < numbers.length; i++){
-multiplied.push(numbers[i] * 4)
+// var numbers=[2,4,6,8]
+// var multiplied=[]
+// for (var i = 0; i < numbers.length; i++){
+// multiplied.push(numbers[i] * 4)
 
-}
-console.log(multiplied)
+// }
+// console.log(multiplied)
 
 
 // print out numbers from 0 to 20....
     
- let n = 0;
+//  let n = 0;
 
-while (n <= 20) {
-  console.log(n);
-  n++;
-}
+// while (n <= 20) {
+//   console.log(n);
+//   n++;
+// }
 
-console.log('')
+// console.log('')
+
+// split and reverse the following string: Welcome to this javascript Guide!and joint it.
+// var string = 'Welcome to this javascript Guide!';
+// var k = string.split('');
+// var reversedArr = []
+// for(var i = k.length; i >= 0; i--) {
+//     reversedArr.push(k[i])
+// }
+// var reversedStr = reversedArr.join('')
+// console.log(reversedStr)
+
+
+
+
+// Use map to return an array all the values in the original array: ["MacD","Pizza",1,4,6,9,"Pie"]
+var arr = ["MacD","Pizza",1,4,6,9,"Pie"];
+var x = arr.map("")
